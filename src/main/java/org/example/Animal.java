f83b83f0ece659b8319752756df3a637bd4dbe71
Dmitry Zhegalov <dmitryzhee@gmail.com>
2023-08-06 22:29:53 +0300

    First commit

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public enum Animal {
+
+  DOG,
+  CAT,
+  HORSE,
+  COW,
+  NOT_DEFINED
+
+}
---
+++ src/main/java/org/example/AnimalFarm.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class AnimalFarm {
+
+  HashMap<Animal, String> farmAnimals= new HashMap<>();
+
+  public void AnimalFarm (HashMap farmAnimals) {
+    this.farmAnimals = farmAnimals;
+  }
+
+  public void countedAnimals () {
+
+  }
+
+}
