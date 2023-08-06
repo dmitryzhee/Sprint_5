a3f23565b3307a74a4cc56d9d5940373ba71ff23
Dmitry Zhegalov <dmitryzhee@gmail.com>
2023-08-06 22:44:39 +0300

    Second commit

--- src/main/java/org/example/AnimalFarm.java
+++ src/main/java/org/example/AnimalFarm.java
@@ -4,15 +4,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class AnimalFarm {
-
+ int i;
   HashMap<Animal, String> farmAnimals= new HashMap<>();
 
   public void AnimalFarm (HashMap farmAnimals) {
     this.farmAnimals = farmAnimals;
   }
 
-  public void countedAnimals () {
 
-  }
 
 }
